dc1ba98596c1e4c98445e44ebbfb3f7d88222698
priyanka <priyankas2k1@gmail.com>
2018-12-16 22:59:40 -0800

    This a runcake test2

---
+++ runcakes.java
@@ -0,0 +1,17 @@
+package dgroupid.restwithcucumber;
+
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//features= {"src/main/resource/features"},
+//glue={"stepdeftn"},
+//)
+
+public class runcakes {
+	
+}
